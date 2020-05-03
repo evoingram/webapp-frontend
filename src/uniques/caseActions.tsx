f98767fb0955955a9e036af1f144283121d2b40c
Erica L. Ingram <inquiries@aquoco.co>
2020-05-02 22:16:44 -0700

    added typescript to caseactions.tsx buttons

--- src/uniques/caseActions.tsx
+++ src/uniques/caseActions.tsx
@@ -5,14 +5,20 @@ import '../css/mainWindow.css';
 function CaseActions() {
 	let helloWorld: string;
 	helloWorld = 'case actions';
+	let addHDate: string;
+	addHDate = 'Add Hearing Date';
+	let addApp: string;
+	addApp = 'Add Appearance';
+	let contactUs: string;
+	contactUs = 'Contact us about this case';
 
 	return (
 		<div className="mainWindow border">
 			<div className="App">
 				<p>{helloWorld}</p>
-				<button>Add Hearing Date</button>
-				<button>Add Appearance</button>
-				<button>Contact us about this case</button>
+				<button>{addHDate}</button>
+				<button>{addApp}</button>
+				<button>{contactUs}</button>
 				<p>&nbsp;</p>
 			</div>
 		</div>
